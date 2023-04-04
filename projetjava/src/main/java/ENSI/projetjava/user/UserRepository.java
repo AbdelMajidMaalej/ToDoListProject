184c24955a75611605ebb1d9668e376c5658d919
AbdelMajidMaalej <abdelmajid.maalej@ensi-uma.tn>
2023-04-04 15:31:01 +0100

    First commit

---
+++ projetjava/src/main/java/ENSI/projetjava/user/UserRepository.java
@@ -0,0 +1,13 @@
+package ENSI.projetjava.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ENSI.projetjava.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("SELECT u FROM User u WHERE u.email = ?1")
+	public User findByEmail(String email);
+
+}
