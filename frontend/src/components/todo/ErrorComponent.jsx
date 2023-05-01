d54d4a28f8bc101bd0b05fcb5b5c770b7676f511
AbdelMajidMaalej <abdelmajid.maalej@ensi-uma.tn>
2023-05-02 00:08:20 +0100

    front

---
+++ frontend/src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,12 @@
+function ErrorComponent() {
+    return (
+        <div className="ErrorComponent">
+            <h1>You need to login!</h1>
+            <div>
+                Error 404 ! <br/>Page not found
+            </div>
+        </div>
+    )
+}
+
+export default ErrorComponent
